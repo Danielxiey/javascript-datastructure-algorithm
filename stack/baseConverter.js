5692ccf2312a6ef4187b7e05e2b0b1682eead996
Daniel Xie <xiedaniel@outlook.com>
2019-08-29 11:06:33 +0800

    用栈实现十进制转任意进制(2-36)

---
+++ stack/baseConverter.js
@@ -0,0 +1,31 @@
+import {Stack} from './stack';
+
+function baseConverter(decNum, base) {
+  const remStack = new Stack();
+  let num = decNum;
+  let rem;
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  if(!(base >= 2 && base <= 36)) {
+    return '';
+  }
+
+  while(num > 0) {
+    rem = Math.floor(num % base);
+    remStack.push(rem);
+    num = Math.floor(num / base);
+  }
+
+  let baseString = '';
+  while(!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()];
+  }
+  return baseString;
+}
+
+//测试
+console.log(baseConverter(100345, 2));
+console.log(baseConverter(100345, 8));
+console.log(baseConverter(100345, 16));
+console.log(baseConverter(100345, 35));
+
